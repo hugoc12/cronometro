84d40b7d23247bc87978be869cd7fb291c6c954c
hugoc12 <hugo_c12@outlook.com>
2022-06-20 01:47:35 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import './App.css';
 
 //#112B3C
@@ -6,37 +6,104 @@ import './App.css';
 //#205375
 //#EFEFEF
 
-const frases = [
-  'A vida trará coisas boas se tiver paciência',
-  'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si',
-  'Não compense na ira o que lhe falta na razão',
-  'Defeitos e virtudes são apenas dois lados da mesma moeda',
-  'A maior de todas as torres começa no solo',
-  'Não há que ser forte. Há que ser flexível',
-  'O verdadeiro homem mede a sua força, quando se defronta com o obstáculo',
-  'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida',
-  'A juventude não é uma época da vida, é um estado de espírito',
-  'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.'
-]
-
-
-
-
 function App(){
-  const [frase, setFrase] = useState('...');
+  const [hr, setHr] = useState('00');
+  const [min, setMin] = useState('00');
+  const [sec, setSec] = useState('00');
+  const [mil, setMil] = useState('00');
 
-  function sortFrase(){
-    let fraseSort = frases[Math.floor(Math.random()*frases.length)];
-    setFrase(fraseSort)
+  const [cronometroStart, setCronometro] = useState(false);
+
+  let myInterval = useRef(null);
+
+  const [ranking, setRanking] = useState([
+  ]);
+
+  function marcarTempo(hr, min, sec, mil){
+    console.log(`${hr} - ${min} - ${sec} - ${mil}`);
+    setRanking([...ranking, `${hr}:${min}:${sec},${mil}`])
+  }
+
+  function start(){
+    setCronometro(true);
+
+    setHr('00');
+    setMin('00');
+    setSec('00');
+    setMil('00');
+
+    setRanking([]);
+
+    let mil = 0;
+    let sec = 0;
+    let min = 0;
+    let hr = 0;
+
+    myInterval.current = setInterval(()=>{
+      console.log(mil);
+      mil+=1; //MILISEGUNDOS
+      if(mil < 10){
+        setMil('0' + mil);
+      }else if(mil == 100){
+        mil = 0;
+        setMil('00');
+        sec+=1; //SEGUNDOS
+        if(sec < 10){
+          setSec('0' + sec);
+        }else if(sec == 60){
+          sec = 0;
+          setSec('00');
+          min+=1 //MINUTOS
+          if(min < 10){
+            setMin('0' + min);
+          }else if(min == 60){
+            min = 0;
+            setMin('00');
+            hr+=1 //HORAS
+
+            if(hr < 10){
+              setHr('0' + hr);
+            }else{
+              setHr(hr);
+            }
+          }else{
+            setMin(min);
+          }
+        }else{
+          setSec(sec);
+        }
+      }else{
+        setMil(mil);
+      }
+
+    }, 10)
+  }
+
+  function stop(){
+    clearInterval(myInterval.current);
+    setCronometro(false);
   }
 
   return(
     <div className='container'>
-      <h1 className='sorte'>{frase}</h1>
+      <h1 className='cronometro'>{hr}:{min}:{sec}<span>,{mil}</span></h1>
+      <button className='btt btt-2' onClick={()=>marcarTempo(hr, min, sec, mil)}>MARCAR</button>
       <div className='containerBtt'>
-        <button className='btt btt-1' onClick={()=>sortFrase()}>OPEN</button>
-        <button className='btt btt-2' onClick={()=>setFrase('...')}>RESET</button>
+        {cronometroStart ? 
+        <button className='btt btt-3' onClick={()=>stop()}>STOP</button> 
+        : 
+        <button className='btt btt-1' onClick={()=>start()}>START</button>
+        }
+        
+        
       </div>
+
+      <ul className='ranking'>
+        {ranking.map((temp, ind)=>{
+          return <li key={ind}>{ind + 1} - {temp}</li>
+        })}
+      </ul>
+      
     </div>
   )
 }
